e13da47da869019d8dd32c3309b94cc25e623b0a
sbx_user1051 <sbx_user1051@169.254.55.7>
2025-01-19 23:27:54 +0000

    Sun Jan 19 23:27:54 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,13 +19,15 @@ def checkout(skus: str):
         "A": 50,
         "B": 30,
         "C": 20,
-        "D": 15
+        "D": 15,
+        "E": 40
     }
     
     # Special Offers.
     special_offers = {
-        "A": (3, 130),
-        "B": (2, 45)
+        "A": [(3, 130), (5, 200)],
+        "B": [(2, 45)],
+        "E": [(2, "B")] # 2E, you get one B free.
     }
     
     # Checking valid input.
@@ -47,9 +49,19 @@ def checkout(skus: str):
     for sku, count in skus_counts.items():
         if sku in special_offers:
             # Special offers.
-            offer_count, offer_price = special_offers[sku]
-            total += (count // offer_count) * offer_price
-            total += (count % offer_count) * prices[sku]
+            for offer in sorted(special_offers[sku], key=lambda x: x[1], reverse=True):
+                if offer[1] == "B":
+                    # 2E, you get one B free.
+                    free_b = count // 2
+                    total += (count // 2) * prices["E"]
+
+                    b = skus_counts.get('B', 0)
+                    skus_counts['B'] = max(0, b - free_b)
+                    break # moving to the next item, once special offer for E is applied.
+                else:   
+                    offer_count, offer_price = special_offers[sku]
+                    total += (count // offer_count) * offer_price
+                    total += (count % offer_count) * prices[sku]
         else:
             # No special offers.
             total += count * prices[sku]
@@ -58,3 +70,4 @@ def checkout(skus: str):
     
     
     
+
