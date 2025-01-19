3bff2f61e0f5d498d5210cd1566c607e5af3885b
sbx_user1051 <sbx_user1051@169.254.13.217>
2025-01-19 21:55:20 +0000

    Sun Jan 19 21:55:20 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int):
     if x and y:
-        if x > -1 and y > -1:
+        if x > 0 and y > 0:
             return x + y
     else:
         raise Exception
